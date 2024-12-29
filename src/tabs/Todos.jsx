9674b2c0b99ebd3929afe24973bb78b562da471d
Igor <pa220282kiv@gmail.com>
2024-12-29 16:18:43 +0200

    Add step 3

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import Text from '../components/Text/Text';
+import { nanoid } from 'nanoid';
+
+const initial = { text: '', id: '' };
 
 const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+  const [todos, setTodos] = useState(initial);
+    const addTodo = (text) => {
+    const newTodo = {text:text, id: nanoid()}
+    setTodos(prev => [...prev, newTodo]);
+  }
+  const deleteTodo = id => {setTodos(prev => prev.filter(item => item.id !== id));}
+
+  return <></>;
 };
 
 export default Todos;
